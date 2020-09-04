ad6e817511f88aa5e487c440c9c245c4ffb159a0
Nguyen Thi Thuy <taylor.thuyng@gmail.com>
2020-09-04 07:27:15 +0700

    edit browser, base test

---
+++ src/test/java/supports/Browser.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Browser {
+
+    public static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void openBrowser(String name) throws IllegalStateException {
+        switch (name.toLowerCase()) {
+            case "ie":
+                driver = new InternetExplorerDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            case "safari":
+                driver = new SafariDriver();
+                break;
+            default:
+                throw new IllegalStateException("Undefine browser : " + name);
+        }
+    }
+
+    public static void main(String [] args){
+        openBrowser("chrome");
+    }
+
+}
